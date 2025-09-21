6896feb8bf6f7d28863fba05ed830fa658b48a7a
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-09-21 18:59:16 +0530

    Backend Project Created

---
+++ finlytics-backend/src/main/java/com/finlytics/api/FinlyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finlytics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlyticsBackendApplication.class, args);
+	}
+
+}
